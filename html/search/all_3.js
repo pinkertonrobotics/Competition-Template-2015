63fdc4486bdf8421d87219a382f3fbb85421357b
Sean Kelley <sgtkode01@gmail.com>
2015-09-10 22:27:37 -0400

    removed doxygen from competition code

--- html/search/all_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['pinkerton_20robotics_20competition_20code_20template_202015',['Pinkerton Robotics Competition Code Template 2015',['../md__media_sean_Documents_Sean_Documents_Robotics_competition-template-2015_README.html',1,'']]],
-  ['pre_5fauton',['pre_auton',['../main_8c.html#ab532f0b1474225e81ebd23b2d05f7c11',1,'main.c']]]
-];
